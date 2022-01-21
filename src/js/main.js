e2e02d681ee04cc98ddb8239e12f5dd5f161d678
MelentQ <mejleht@yandex.ru>
2022-01-21 18:17:29 +0300

    footer added

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
-import polyfills from './build-in/polyfills';
+// import polyfills from './build-in/polyfills';
 import './build-in/lazyload';
 import detectTouch from './build-in/detectTouch';
 import setScrollbarWidth from './build-in/setScrollbarWidth';
-import validation from './build-in/validation';
-import customSelects from './build-in/customSelects';
-import masks from './build-in/masks';
-import fileUpload from './build-in/fileUpload';
+// import validation from './build-in/validation';
+// import customSelects from './build-in/customSelects';
+// import masks from './build-in/masks';
+// import fileUpload from './build-in/fileUpload';
 import anchorLinks from './build-in/anchorLinks';
-import mediaPlayer from './build-in/mediaPlayer';
-import datepicker from './build-in/datepicker';
-import accordions from './build-in/accordions';
-import modals from './build-in/modals';
+// import mediaPlayer from './build-in/mediaPlayer';
+// import datepicker from './build-in/datepicker';
+// import accordions from './build-in/accordions';
+// import modals from './build-in/modals';
 
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -26,18 +26,18 @@ import initAnimations from './custom/initAnimations';
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener('DOMContentLoaded', function () {
-    polyfills();
+    // polyfills();
     detectTouch();
     setScrollbarWidth();
-    validation();
-    customSelects();
-    masks();
-    fileUpload();
+    // validation();
+    // customSelects();
+    // masks();
+    // fileUpload();
     anchorLinks();
-    accordions();
-    mediaPlayer();
-    modals();
-    datepicker();
+    // accordions();
+    // mediaPlayer();
+    // modals();
+    // datepicker();
 
     // custom
 
